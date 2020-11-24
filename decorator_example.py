c0f75105484e87f7d64bbe0f5014844be08a1f41
Kanchan Garg <kanchangarg0724@gmail.com>
2020-11-24 22:55:28 +0530

    Add files via upload

---
+++ decorator_example.py
@@ -0,0 +1,40 @@
+# ================== One way of call decorators ============================= #
+
+
+def decorate_message(fun):  # Decorator accepts a function as an argument
+    # Inner function
+    def addWelcome(message):  # Value of text argument in display function will copy here
+        return "Welcome to " + fun(message)
+
+    # Decorator returns a function
+    return addWelcome
+
+
+@decorate_message
+def display(text):
+    return text
+
+
+print(str(display("Python")))
+
+# ================== Other way of call decorators ============================= #
+
+
+def decorate_function(fun):  # Decorator accepts a function as an argument
+    # Inner function
+    def swap(x,y):  # Value of a and b arguments in div function will copy here
+        if x < y:
+            x,y = y,x
+        return fun(x,y)
+
+    # Decorator returns a function
+    return swap
+
+
+def div(a,b):
+    return a/b
+
+# You can also add @decorate_function before the definition as well
+div1 = decorate_function(div)
+
+print(div1(2,4))
